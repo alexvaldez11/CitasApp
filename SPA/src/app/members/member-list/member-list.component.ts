8d94624de200138f82fd01d48f826f693a6d0011
Alejandro Valdez <alexvaldezcasillas111@gmail.com>
2024-02-27 12:42:02 -0600

    Storing the state

--- SPA/src/app/members/member-list/member-list.component.ts
+++ SPA/src/app/members/member-list/member-list.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { IMember } from 'src/app/_models/imember';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members: IMember[] = [];
-  
-  constructor(private membersService: MembersService) { }
+  members$?: Observable<IMember[]>;
+
+  constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.membersService.getMembers();
   }
-
-  loadMembers() {
-    this.membersService.getMembers().subscribe({
-      next: response => this.members = response
-    })
-  }
-
 }
