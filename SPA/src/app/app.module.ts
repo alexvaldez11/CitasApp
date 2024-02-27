8c1e148d0786c89c39cc9dcf421ca4ecc3227fe4
Alejandro Valdez <alexvaldezcasillas111@gmail.com>
2024-02-27 12:40:22 -0600

    SPA loading

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { MemberEditComponent } from './member-edit/member-edit.component';
 
 @NgModule({
@@ -46,6 +47,7 @@ import { MemberEditComponent } from './member-edit/member-edit.component';
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
